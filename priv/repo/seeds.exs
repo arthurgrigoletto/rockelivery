390cd34b565116af0aa57280d86d7f09ba11ed0f
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2021-04-17 11:21:48 -0300

    finalizando o modulo

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,44 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{Item, Order, Repo, User}
+
+user = %User{
+  age: 27,
+  address: "Rua Hungria, 574",
+  cep: "12345678",
+  cpf: "12345678900",
+  email: "arthur@gmail.com",
+  password: "123456",
+  name: "Arthur"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1 = %Item{
+  category: :food,
+  description: "banana frita",
+  price: Decimal.new("15.50"),
+  photo: "priv/photo/banana_frita.png"
+}
+
+item2 = %Item{
+  category: :food,
+  description: "banana cozida",
+  price: Decimal.new("10.50"),
+  photo: "priv/photo/banana_cozida.png"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item2, item2],
+  address: "Rua Hungria, 574",
+  comments: "sem canela",
+  payment_method: :money
+}
+
+Repo.insert!(order)
